80c5b6e18954c1db8f9275430f5cee93c3cce5b0
Cl3ment <cl3mentdev@gmail.com>
2014-10-03 22:10:44 +0200

    Premier commit

---
+++ mail.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: cp1252 -*-
+
+from smtplib import SMTP
+import sys
+
+#meilleurInput reprend input() mais supporte les caractères spéciaux sur Python 2.x
+#sur Python 3.x, utilisez la fonction input() normale.
+def meilleurInput(unMessage):
+	print unMessage,
+	chaine = sys.stdin.readline().strip()
+	return chaine
+
+
+malheureuxDestinataire = meilleurInput(" Entrez le mail du malheureux destinataire : ")
+username = meilleurInput("Entrez votre nom d'utilisateur GMail : ")
+passwd = meilleurInput("Entrez votre mot de passe : ")
+message = meilleurInput("Entrez le corps du message : ")
+
+session = SMTP('smtp.gmail.com', 587)
+session.ehlo()
+session.starttls()
+
+session.login(username, passwd)
+
+msg = "\n" + message
+#\n sépare le corps du message des headers qui le précèdent
+
+n = int(meilleurInput("Entrez le nombre de répétitions : "))
+print "Veuillez patienter"
+
+for i in range(0, n):
+	session.sendmail(username + "@gmail.com", malheureuxDestinataire, msg)
